7b1d4e2c73b196c704ca16b2c56566af2bd78743
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-24 11:16:11 -0500

    fleshed out index and App in src folder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import PageContainer from "./components/PageContainer";
+import Footer from './components/Footer'
+
+
+const App = () => {
+
+    return (
+
+        <div>
+            <PageContainer /> <Footer />
+        </div>
+
+    )
+
+};
+
+export default App;
--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -5,6 +5,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 
+
 export default function PageContainer() {
 
     const [currentPage, setCurrentPage] = useState('About');
