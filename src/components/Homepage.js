5bc8ff1290e33288db2cb52b475368e54e909946
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-26 16:22:56 -0500

    added js and css for homepage to load upon initial visit

---
+++ src/components/Homepage.js
@@ -0,0 +1,19 @@
+import './assets/Homepage.css'
+
+export default function Welcome () {
+
+    return (
+        
+        <div className='homepage-container'>
+
+            <h1 className='homepage-title'>Welcome</h1>
+            <hr />
+            <p className='homepage-message'>Hey there!  My name is Kellen Andrea!  I'm a Junior Full-Stack Developer.
+            I have created this portfolio to display projects that I have built, as well as cursory information about me.
+            Please peruse my work, and contact me if you have questions!
+            </p>
+
+        </div>
+            
+    )
+}
