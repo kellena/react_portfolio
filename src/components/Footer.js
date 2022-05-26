6ecc4f25027a40f3c701186004af572c1eddb17d
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-26 18:08:11 -0500

    fleshed out footer, linked images for contact info

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import './assets/Footer.css'
+
+export default function Footer () {
+
+    return (
+
+        <footer>
+
+        <a href="https://github.com/kellena">
+            <img className="link-tree-logo"
+            src={require("../assets/img/github-icon.png")}
+            alt="github icon"
+        /></a>
+
+        <a href="https://www.linkedin.com/in/kellen-andrea-359b2583/">
+            <img
+            className="link-tree-logo"
+            src={require("../assets/img/linkedin-icon.png")}
+            alt="linkedIn icon"
+        /></a>
+
+        <a href="mailto:kellen.andrea.416@gmail.com">
+            <img
+            className="link-tree-logo"
+            src={require("../assets/img/email-icon.png")}
+            alt="email icon"
+        /></a>
+
+        <a href="tel:+6512955639"
+          ><img
+            className="link-tree-logo"
+            src={require("../assets/img/phone-icon.png")}
+            alt="phone icon"
+        /></a>
+
+        </footer>
+
+    );
+
+};
